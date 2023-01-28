8f9d2936561c24c31cb3e48744272252536dad3e
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-28 00:06:55 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,13 +4,19 @@ echo "[ups]" > /etc/nut/ups.conf
 echo "driver = $UPS_DRIVER" >> /etc/nut/ups.conf
 echo "port = $UPS_PORT" >> /etc/nut/ups.conf
 
-echo "[$UPS_USER]" > /etc/nut/upsd.users
-echo "password = $UPS_PASSWORD" >> /etc/nut/upsd.users
-echo "upsmon primary" >> /etc/nut/upsd.users
-echo "actions = SET" >> /etc/nut/upsd.users
-echo "instcmds = ALL" >> /etc/nut/upsd.users
+echo "LISTEN 0.0.0.0 3493" > /etc/nut/upsd.conf
 
-echo "MONITOR ups@localhost 1 $UPS_USER $UPS_PASSWORD primary" > /etc/nut/upsmon.conf
+echo "[$ADMIN_USER]" > /etc/nut/upsd.users
+echo "password = $ADMIN_PASSWORD" >> /etc/nut/upsd.users
+echo "actions = set" >> /etc/nut/upsd.users
+echo "actions = fsd" >> /etc/nut/upsd.users
+echo "instcmds = all" >> /etc/nut/upsd.users
+echo "" >> /etc/nut/upsd.users
+echo "[$API_USER]" >> /etc/nut/upsd.users
+echo "password = $API_PASSWORD" >> /etc/nut/upsd.users
+echo upsmon master
+
+echo "MONITOR ups@localhost 1 $API_USER $API_PASSWORD master" > /etc/nut/upsmon.conf
 
 # Change perms of nut run dirs
 chown -R nut:nut /dev/bus/usb /var/run/nut
