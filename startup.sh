c27046b27ec9ba8417824057914997b4db37193a
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-28 00:57:45 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,18 +11,15 @@ echo "pollinterval = 15" >> /etc/nut/ups.conf
 echo "LISTEN 0.0.0.0 3493" > /etc/nut/upsd.conf
 
 # SETUP USERS
-echo "[$ADMIN_USER]" > /etc/nut/upsd.users
-echo "password = $ADMIN_PASSWORD" >> /etc/nut/upsd.users
+echo "[$UPS_USER]" > /etc/nut/upsd.users
+echo "password = $UPS_PASSWORD" >> /etc/nut/upsd.users
 echo "actions = set" >> /etc/nut/upsd.users
 echo "actions = fsd" >> /etc/nut/upsd.users
 echo "instcmds = all" >> /etc/nut/upsd.users
-#echo "" >> /etc/nut/upsd.users
-#echo "[$API_USER]" >> /etc/nut/upsd.users
-#echo "password = $API_PASSWORD" >> /etc/nut/upsd.users
 echo "upsmon master" >> /etc/nut/upsd.users
 
 # SETUP MONITORING
-echo "MONITOR ups@localhost 1 $ADMIN_USER $ADMIN_PASSWORD master" > /etc/nut/upsmon.conf
+echo "MONITOR ups@localhost 1 $UPS_USER $UPS_PASSWORD master" > /etc/nut/upsmon.conf
 
 # FIX PERMISSIONS
 chown -R nut:nut /dev/bus/usb /var/run/nut
