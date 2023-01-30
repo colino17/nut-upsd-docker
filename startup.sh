9e686238458fa741950112ec379790139db14252
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-30 12:04:31 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,19 +13,19 @@ echo "LISTEN $REMOTE_IP 3493" >> /etc/nut/upsd.conf
 echo "MAXAGE 25" >> /etc/nut/upsd.conf
 
 # SETUP USERS
-echo "[local]" > /etc/nut/upsd.users
-echo "password = local" >> /etc/nut/upsd.users
-echo "actions = set" >> /etc/nut/upsd.users
-echo "actions = fsd" >> /etc/nut/upsd.users
-echo "instcmds = all" >> /etc/nut/upsd.users
-echo "upsmon primary" >> /etc/nut/upsd.users
-echo "" >> /etc/nut/upsd.users
+#echo "[local]" > /etc/nut/upsd.users
+#echo "password = local" >> /etc/nut/upsd.users
+#echo "actions = set" >> /etc/nut/upsd.users
+#echo "actions = fsd" >> /etc/nut/upsd.users
+#echo "instcmds = all" >> /etc/nut/upsd.users
+#echo "upsmon primary" >> /etc/nut/upsd.users
+#echo "" >> /etc/nut/upsd.users
 echo "[$UPS_USER]" >> /etc/nut/upsd.users
 echo "password = $UPS_PASSWORD" >> /etc/nut/upsd.users
-echo "upsmon secondary" >> /etc/nut/upsd.users
+echo "upsmon primary" >> /etc/nut/upsd.users
 
 # SETUP MONITORING
-echo "MONITOR ups@localhost 1 local local primary" > /etc/nut/upsmon.conf
+echo "MONITOR ups@localhost 1 $UPS_USER $UPS_PASSWORD primary" > /etc/nut/upsmon.conf
 
 # FIX PERMISSIONS
 chown -R nut:nut /dev/bus/usb /var/run/nut
