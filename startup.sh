f92ff00b691d03289d62f04de1bcb39185c0042f
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-30 11:18:56 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,17 +9,23 @@ echo "pollinterval = 15" >> /etc/nut/ups.conf
 
 # ALLOW API ACCESS
 echo "LISTEN 0.0.0.0 3493" > /etc/nut/upsd.conf
+echo "MAXAGE 25" > /etc/nut/upsd.conf
 
 # SETUP USERS
-echo "[$UPS_USER]" > /etc/nut/upsd.users
-echo "password = $UPS_PASSWORD" >> /etc/nut/upsd.users
+echo "[local]" > /etc/nut/upsd.users
+echo "password = local" >> /etc/nut/upsd.users
 echo "actions = set" >> /etc/nut/upsd.users
 echo "actions = fsd" >> /etc/nut/upsd.users
 echo "instcmds = all" >> /etc/nut/upsd.users
-echo "upsmon master" >> /etc/nut/upsd.users
+echo "upsmon primary" >> /etc/nut/upsd.users
+echo "" >> /etc/nut/upsd.users
+echo "[$UPS_USER]" >> /etc/nut/upsd.users
+echo "password = $UPS_PASSWORD" >> /etc/nut/upsd.users
+echo "upsmon secondary" >> /etc/nut/upsd.users
+
 
 # SETUP MONITORING
-echo "MONITOR ups@localhost 1 $UPS_USER $UPS_PASSWORD master" > /etc/nut/upsmon.conf
+echo "MONITOR ups@localhost 1 local local primary" > /etc/nut/upsmon.conf
 
 # FIX PERMISSIONS
 chown -R nut:nut /dev/bus/usb /var/run/nut
